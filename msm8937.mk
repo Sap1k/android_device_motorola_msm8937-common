5c326ab9d8262724ed4d07ba3cb0a8664bb2c526
Jarl-Penguin <jarlpenguin@outlook.com>
2020-06-16 03:18:28 +0300

    msm8937-common: Drop AOSP camera provider service
    * We use Moto's camera provider service
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I44948c076ff51f5f2ab588bb8037929b38a2c6a1

--- msm8937.mk
+++ msm8937.mk
@@ -83,8 +83,7 @@ PRODUCT_PACKAGES += \
     libwui
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.4-impl
 
 # CNE
 PRODUCT_PACKAGES += \
